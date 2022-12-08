8ed1f28a96a1e1c16f1a2808acf0dfe7e3c104b8
Topher McCulloch <tophermcculloch@gmail.com>
2022-12-07 21:01:05 -0600

    Day 7 strugglebus

--- js/day07.js
+++ js/day07.js
@@ -45,35 +45,61 @@ function parseCommands( output ) {
     return commandArr;
 }
 
-console.log(parseCommands( outputArray ));
-
+let outputArrayParsed = parseCommands( outputArray );
 
 function parseOutput( output ) {
-    let filesystem = [];
+    // let filesystem = { path: '/', children: []};
+    let filesystem = ['/'];
 
     output.forEach((line, i) => {
-        let lineParse = line.split(" ");
+        console.log(i);
+        let lineParse = line[0].split(" ");
         let directoryPointer = 0;
+        let cwd = "/";
 
-        // Running a command
-        if ( lineParse[0] === "$" ) {
-            // change directory
-            if ( lineParse[1] === "cd" ) {
-                if ( lineParse[2] === "/" ) {
-                    directoryPointer = 0;
-                } else if ( lineParse[2] === ".." ) {
-                    directoryPointer = directoryPointer - 1;
+        // change directory
+        if ( lineParse[1] === "cd" ) {
+            if ( lineParse[2] === "/" ) {
+                directoryPointer = 0;
+                cwd = "/";
+            } else if ( lineParse[2] === ".." ) {
+                directoryPointer = directoryPointer - 1;
+            } else {
+                // this isn't gonna work 
+                cwd = lineParse[2];
+                console.log('plot ' + cwd);
+                // do a search based on cwd to target array to make nested 
+                if ( cwd === "/" || directoryPointer === 0) {
+                    if (!filesystem.includes(cwd)) {
+                        console.log('poot1');
+                        filesystem.push(cwd);
+                    }
                 } else {
-                    // this isn't gonna work 
-                    directoryPointer === lineParse[2];
+                    if (!filesystem[directoryPointer].includes(cwd)) {
+                        console.log('poot2 ' + directoryPointer);
+                        filesystem[directoryPointer].push(cwd);
+                    }
                 }
-            // list files
-            } else if ( lineParse[1] === "ls" ) {
-                for ( let j = 0; j < output.length; j++) {
-                    let pointer = (j + i) % output.length;
-                    // run this until next $
-                    // probably more efficient to split array sooner
-                    // console.log(output[pointer]);
+            }
+        // list files
+        } else if ( lineParse[1] === "ls" ) {
+            console.log({i});
+            for ( let j = 0; j < line.length; j++) {
+                
+                if ( j > 1 ) {
+                    // why is ls getting added
+                    console.log({j});
+                    // console.log(filesystem);
+                    // console.log(filesystem[0]);
+                    if ( cwd === "/") {
+                        if (!filesystem.includes(line)) {
+                            filesystem.push(line);
+                        }
+                    } else {
+                        if (!filesystem[directoryPointer].includes(line)) {
+                            filesystem[directoryPointer].push(line);
+                        }
+                    }
                 }
             }
         }
@@ -83,4 +109,62 @@ function parseOutput( output ) {
     return filesystem;
 }
 
+console.log(parseOutput(outputArrayParsed));
 
+// [ {dir: a, } ]
+
+// - / (dir)
+//   - a (dir)
+//     - e (dir)
+//       - i (file, size=584)
+//     - f (file, size=29116)
+//     - g (file, size=2557)
+//     - h.lst (file, size=62596)
+//   - b.txt (file, size=14848514)
+//   - c.dat (file, size=8504156)
+//   - d (dir)
+//     - j (file, size=4060174)
+//     - d.log (file, size=8033020)
+//     - d.ext (file, size=5626152)
+//     - k (file, size=7214296)
+
+// const TreeNode = {
+//     path: 'test/fixtures',
+//     children: [
+//       TreeNode {
+//         path: 'test/fixtures/example.js',
+//         children: []
+//       },
+//       TreeNode {
+//         path: 'test/fixtures/utils',
+//         children: [Array]
+//       }
+//     ]
+//   }
+
+//   const home = {
+//     path: '/',
+//     children: [
+//         { 
+//             name: 'b.text',
+//             size: 13333,
+//         }, 
+//         {
+//             name: 'c.dat',
+//             size: 34444
+//         },
+//         {
+//             path: 'd',
+//             children: [
+//                 { 
+//                     name: 'b.text',
+//                     size: 13333,
+//                 }, 
+//                 {
+//                     name: 'c.dat',
+//                     size: 34444
+//                 }
+//             ]
+//         }
+//     ]
+//   }
