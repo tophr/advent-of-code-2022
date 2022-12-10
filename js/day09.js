f0d0ee534016cf78051e7dd6b809fa221bc73dab
Topher McCulloch <tophermcculloch@gmail.com>
2022-12-09 21:32:35 -0600

    Starts day 9 with non-working AI solution

---
+++ js/day09.js
@@ -0,0 +1,69 @@
+const inputEx = `R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`;
+
+// const input = ``;
+
+const input = inputEx.split("\n");
+
+// initialize the head and tail positions
+let head_x = 0;
+let head_y = 0;
+let tail_x = 0;
+let tail_y = 0;
+
+// initialize a dictionary to keep track of visited positions
+let visited = {};
+
+// initialize a counter for the number of visited positions
+let num_visited = 0;
+
+// loop through each movement in the input
+input.forEach((movement, i) => {
+    // this isn't working because it needs to step one at a time in moves 
+    // parse the direction and distance from the movement
+    movement = movement.split(" ");
+    let direction = movement[0];
+    let distance = parseInt(movement[1]);
+    // console.log(movement);
+    // move the head in the specified direction
+    if ( direction == "U") {
+        head_y += distance;
+    } else if ( direction == "D" ) {
+        head_y -= distance;
+    } else if ( direction == "L" ) {
+        head_x -= distance;
+    } else if ( direction == "R" ) {
+        head_x += distance;
+    }
+
+    // don't think this is accounting for the diagonal rule 
+    // update the tail position if necessary
+    if ( Math.abs(head_x - tail_x) > 1 || Math.abs(head_y - tail_y) > 1 ) {
+        if ( head_x > tail_x ) {
+            tail_x += 1;
+        } else if ( head_x < tail_x ) {
+            tail_x -= 1;
+        } else if ( head_y > tail_y ) {
+            tail_y += 1;
+        } else if ( head_y < tail_y ) {
+            tail_y -= 1;
+        }
+    }
+
+    // mark the current head position as visited
+    visited[(head_x, head_y)] = true;
+    num_visited += 1;
+
+    // return the number of visited positions
+    // return num_visited;
+    console.log(head_x + head_y);
+});
+
+console.log(num_visited);
+console.log(visited);
